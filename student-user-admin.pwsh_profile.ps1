33a7c12cc66e692975dd21f308e8ae0cb3523161
Integrated Services <92554763+is-eduadv@users.noreply.github.com>
2025-04-04 11:27:07 +1100

    Delete student-user-admin.pwsh_profile.ps1

--- student-user-admin.pwsh_profile.ps1
+++
@@ -1,9 +0,0 @@
-function Run-AdMgmtScript {
-    & "C:\EA\ad_mgmt_2024.05.ps1"
-}
-
-# Command to run on demand
-Set-Alias -Name "Manage-Users" -Value Run-AdMgmtScript
-
-# Run automatically
-Run-AdMgmtScript
